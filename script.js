29fe3ab9574d491482d5d7059e9b7fc70e109d76
ADIL-FARIDI <adilfaridi07@gmail.com>
2024-02-25 20:29:08 +0530

    Under-Construction

---
+++ script.js
@@ -0,0 +1,17 @@
+function goTo(url) {
+  window.location.href = url;
+}
+
+function goToWhatsapp(url) {
+  window.open(url, "_blank");
+}
+function goToMaps(url) {
+  window.open(url, "_blank");
+}
+
+function goToGmail() {
+  window.open(
+    "https://mail.google.com/mail/?view=cm&fs=1&to=riyaz@mtcl.online",
+    "_blank"
+  );
+}
