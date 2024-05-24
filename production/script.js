c7e732a642d4d4bbba29bceb0403342562a3be07
ADIL-FARIDI <adilfaridi07@gmail.com>
2024-05-25 02:22:39 +0530

    dev-1-navbar

--- production/script.js
+++ production/script.js
@@ -15,3 +15,14 @@ function updateTime() {
 setInterval(updateTime, 1000);
 updateTime();
 ////////////////////////////////
+
+// datecode for topnav
+var currentDate = new Date();
+
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
+
+var formattedDate = day + "/" + month + "/" + year;
+
+document.getElementById("dateDisplay").textContent = formattedDate;
