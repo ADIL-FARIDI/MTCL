1cd0b64a658306c36ff0e56c4faffce3e07b3472
ADIL-FARIDI <adilfaridi07@gmail.com>
2024-05-25 17:03:28 +0530

    dev-2-carosel

--- production/script.js
+++ production/script.js
@@ -26,3 +26,27 @@ var year = currentDate.getFullYear();
 var formattedDate = day + "/" + month + "/" + year;
 
 document.getElementById("dateDisplay").textContent = formattedDate;
+///////////////////////////////////
+
+//image carousel
+let slideIndex = 0;
+let slideTimeout;
+
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+
+  clearTimeout(slideTimeout);
+
+  slideTimeout = setTimeout(showSlides, 3000);
+}
