46822faa1cf08e7757d415023636fbbc80996e0f
ADIL-FARIDI <adilfaridi07@gmail.com>
2024-05-29 02:22:24 +0530

    dev-navbar-animation-done

--- production/js/hamburger.js
+++ production/js/hamburger.js
@@ -1,9 +1,24 @@
-////////////////////////////////////////////////////////////
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.querySelector(".hamburger-menu");
   const dropdownMenu = document.querySelector(".dropdown-menu");
+  const hamburgerIcon = document.querySelector(".hamburger-menu i");
 
   hamburgerMenu.addEventListener("click", function () {
-    dropdownMenu.classList.toggle("show");
+    toggleMenu();
   });
+
+  function toggleMenu() {
+    dropdownMenu.classList.toggle("show");
+    if (dropdownMenu.classList.contains("show")) {
+      hamburgerIcon.classList.remove("fa-bars");
+      hamburgerIcon.classList.add("fa-times");
+    } else {
+      hamburgerIcon.classList.remove("fa-times");
+      hamburgerIcon.classList.add("fa-bars");
+      dropdownMenu.classList.add("hide"); // Add hide class to initiate slide-out animation
+      setTimeout(() => {
+        dropdownMenu.classList.remove("hide"); // Remove hide class after animation completes
+      }, 500); // Adjust timing to match animation duration
+    }
+  }
 });
