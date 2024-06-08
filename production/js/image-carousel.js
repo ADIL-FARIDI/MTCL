cc4a76678a8e78bdb0950534e7e652708aa3acf4
ADIL-FARIDI <adilfaridi07@gmail.com>
2024-06-09 01:32:04 +0530

    dev-carosel-responsive

--- production/js/image-carousel.js
+++ production/js/image-carousel.js
@@ -1,13 +1,18 @@
-//image carousel
 let slideIndex = 0;
 let slideTimeout;
-showSlides();
+
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+  let slides;
+  if (window.innerWidth > 900) {
+    slides = document.getElementsByClassName("mySlides");
+  } else {
+    slides = document.getElementsByClassName("mySlides-mobile");
+  }
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
   if (slideIndex > slides.length) {
     slideIndex = 1;
@@ -15,6 +20,14 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
 
   clearTimeout(slideTimeout);
-
   slideTimeout = setTimeout(showSlides, 3000);
 }
+
+function initCarousel() {
+  slideIndex = 0;
+  clearTimeout(slideTimeout);
+  showSlides();
+}
+
+window.addEventListener("resize", initCarousel);
+document.addEventListener("DOMContentLoaded", initCarousel);
